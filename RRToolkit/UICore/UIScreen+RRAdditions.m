5f06da5dc1ce32b65fed75c42446966e0d7d5c38
renhy <renhy@bitauto.com>
2015-05-27 16:36:53 +0800

    modify

---
+++ RRToolkit/UICore/UIScreen+RRAdditions.m
@@ -0,0 +1,72 @@
+//
+//  UIScreen+Additions.m
+//  NavDemo
+//
+//  Created by Ryan on 15/5/27.
+//  Copyright (c) 2015年 Ryan. All rights reserved.
+//
+
+#import "UIScreen+RRAdditions.h"
+
+@implementation UIScreen (Additions)
+
+CGFloat OnePixel() { return 1.0 / UIScreen.scale; }
+
++ (CGFloat)screenWidth{
+    if (UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)) {
+        if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+            return [UIScreen mainScreen].nativeBounds.size.height / [UIScreen mainScreen].nativeScale;
+        } else {
+            return [UIScreen mainScreen].bounds.size.height;
+        }
+    } else {
+        if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+            return [UIScreen mainScreen].nativeBounds.size.width/[UIScreen mainScreen].nativeScale;
+        } else {
+            return [UIScreen mainScreen].bounds.size.width;
+        }
+    }
+}
+
++ (CGFloat)screenHeight{
+    if (UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)) {
+        if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+            if ([UIApplication sharedApplication].statusBarFrame.size.width > 20) {
+                return [UIScreen mainScreen].nativeBounds.size.width / [UIScreen mainScreen].nativeScale - 20;
+            }
+            return [UIScreen mainScreen].nativeBounds.size.width / [UIScreen mainScreen].nativeScale;
+        } else {
+            if ([UIApplication sharedApplication].statusBarFrame.size.width > 20) {
+                return [UIScreen mainScreen].bounds.size.width - 20;
+            }
+            return [UIScreen mainScreen].bounds.size.width;
+        }
+    } else {
+        if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+            if ([UIApplication sharedApplication].statusBarFrame.size.height > 20) {
+                return [UIScreen mainScreen].nativeBounds.size.height/[UIScreen mainScreen].nativeScale - 20;
+            }
+            return [UIScreen mainScreen].nativeBounds.size.height / [UIScreen mainScreen].nativeScale;
+        } else {
+            if ([UIApplication sharedApplication].statusBarFrame.size.height > 20) {
+                return [UIScreen mainScreen].bounds.size.height - 20;
+            }
+            return [UIScreen mainScreen].bounds.size.height;
+        }
+    }
+}
+
++ (CGFloat)scale{
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+        return [UIScreen mainScreen].nativeScale;
+    } else {
+        return [UIScreen mainScreen].scale;
+    }
+}
+
++ (CGFloat)screenScaleRatio {
+    return [UIScreen screenWidth] / 375.f;
+}
+
+
+@end
