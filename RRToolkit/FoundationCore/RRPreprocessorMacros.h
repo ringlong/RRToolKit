c5f7b012dbd70f5952c3feb9438a8a107c9e98d7
宏宇 <266949@qq.com>
2016-10-18 14:49:40 +0800

    添加 iOS 版本判断

--- RRToolkit/FoundationCore/RRPreprocessorMacros.h
+++ RRToolkit/FoundationCore/RRPreprocessorMacros.h
@@ -40,7 +40,8 @@
 
 #define IsNumber(__number) ([(__number) isKindOfClass:[NSNumber class]])
 
-#define iOS7OrLater    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
 #define iOS8OrLater    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+#define iOS9OrLater    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 9.0)
+#define iOS10OrLater    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 10.0)
 
 #endif //RRFrameworkCore_RRPreprocessorMacros_h
